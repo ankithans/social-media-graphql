c6eea81ef82dbae08675ea191397542e23f83b1a
Ankit Hans <ankithans1947@gmail.com>
2021-02-26 22:56:23 +0530

    feat: init basic graphql server

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const User = require("./models/User");
+const Post = require("./models/Post");
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
